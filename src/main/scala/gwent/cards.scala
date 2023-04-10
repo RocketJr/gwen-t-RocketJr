388e985459b29499daa4a61c8be743d933cd13d2
RocketJr <leo.rikhardsson02@gmail.com>
2023-04-10 19:49:50 -0400

    Creating and finishing the cards and testcards class, also finishing the documentation

--- src/main/scala/gwent/cards.scala
+++ src/main/scala/gwent/cards.scala
@@ -1,6 +1,23 @@
 package cl.uchile.dcc
 package gwent
 
-class cards {
+/**
+ * @param CardName The name of the Card
+ * @param Classify The classification of the card
+ *
+ * @example
+ * {{{
+ * Unit = new cards("Mario the Jhin", "range")
+ * Weather = new cards("The Doom")
+ * }}}
+ *
+ * @constructor Creates a new card with the specified information
+ *
+ * @author Leonardo Rikhardsson
+ * @since 1.0.0
+ * @version 1.0.0
+ */
+
+class cards(val CardName: String, val Classify: String = null) {
 
 }
---
+++ src/test/scala/gwent/testcards.scala
@@ -0,0 +1,24 @@
+package cl.uchile.dcc
+package gwent
+
+import munit.FunSuite
+
+class testcards extends munit.FunSuite{
+  var Unit: cards = _
+  var Weather: cards = _
+
+  override def beforeEach(context: BeforeEach): Unit = {
+    Unit = new cards("Mario the Jhin", "range")
+    Weather = new cards("The Doom")
+  }
+
+  test("A Card needs a name") {
+    assertEquals(Unit.CardName, "Mario the Jhin")
+    assertEquals(Weather.CardName, "The Doom")
+  }
+
+  test("A card needs a classification") {
+    assertEquals(Unit.Classify, "range")
+    assertEquals(Weather.Classify, null)
+  }
+}
