fcaf2cf9ab3c179e33b76fb9ab0c05378457aa48
RocketJr <leo.rikhardsson02@gmail.com>
2023-04-24 23:27:52 -0400

    FEAT adding the documentation of Cards and AbstractCards

--- src/main/scala/gwent/CardType/Cards.scala
+++ src/main/scala/gwent/CardType/Cards.scala
@@ -1,6 +1,18 @@
 package cl.uchile.dcc
 package gwent.CardType
 
+/** A trait representing a Cards with a name, it's class, and power.
+ *
+ * @param CardName The name of the Card.
+ * @param Classify The class of the Card.
+ * @param Empowered The power of the Card.
+ *
+ * @author Leonardo Rikhardsson
+ *
+ * @since 1.0.1
+ * @version 1.0.0
+ */
+
 trait Cards {
   /** The name of the Card. */
   val CardName: String
