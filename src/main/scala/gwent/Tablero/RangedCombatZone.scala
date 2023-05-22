1017d363cfa18573b23fe4ff663f0730e745fa40
RocketJr <leo.rikhardsson02@gmail.com>
2023-05-21 22:58:10 -0400

    FEAT adding new Board and Zone Classes

--- src/main/scala/gwent/Tablero/RangedCombatZone.scala
+++ src/main/scala/gwent/Tablero/RangedCombatZone.scala
@@ -2,5 +2,7 @@ package cl.uchile.dcc
 package gwent.Tablero
 
 class RangedCombatZone {
-
+  override def jugarCarta(carta: Carta, tablero: Tablero): Unit = {
+    tablero.jugarCartaEnZona(carta, this)
+  }
 }
--- src/main/scala/gwent/Tablero/WeatherZone.scala
+++ src/main/scala/gwent/Tablero/WeatherZone.scala
@@ -2,5 +2,6 @@ package cl.uchile.dcc
 package gwent.Tablero
 
 class WeatherZone {
-
+  override def jugarCarta(carta: Carta, tablero: Tablero): Unit = {
+    tablero.jugarCartaEnZona(carta, this)
 }
